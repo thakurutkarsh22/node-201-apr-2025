f722a05dcf5c71e9c3dc42c7afd075b0e7abc75a
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2025-04-02 22:16:53 +0530

    how to get the postman collection

---
+++ PostmanCollection/StepsTOGetTheCollectionToYOurPostman.js
@@ -0,0 +1,8 @@
+/*
+    1. Open Postman 
+    2. Click on Import button next to New button on top left.
+    3. copy Node-201-apr-2025.postman_collection.json and paste it in the field.
+
+    4. you are done with the collection import
+
+*/
