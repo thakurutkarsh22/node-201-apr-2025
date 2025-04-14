a0eb52472c2814dc7d45501ce8ec61b335e515d4
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2025-04-14 21:36:15 +0530

    session7

---
+++ Session7/index.js
@@ -0,0 +1,13 @@
+// PITFALLS WHILE DEVELOPMENT
+// 1. whenever we are using promises we need to be extra creaful to use await , the best thing to debug will be console.logs
+
+// console.log("methodName || before or after xyz line", variable );
+/**
+ * 2.EaddrinUse -> search the internet 
+ * https://stackoverflow.com/questions/4075287/node-express-eaddrinuse-address-already-in-use-how-can-i-stop-the-process
+ * 
+ * 
+ * 3. if mongoDB connection is giving error and not able to connect to DB .
+ * 4. package.lock.json will make sure that project dependencies dependency 
+ * we can change that versions according to our needs
+ */
